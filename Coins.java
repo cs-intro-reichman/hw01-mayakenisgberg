2a8c48ae855262965b75f1bae7d02821ca29788a
Maya Kenigsberg <maya.kenigsberg@post.runi.ac.il>
2023-12-21 13:23:25 +0200

    homework firt attempt

--- Coins.java
+++ Coins.java
@@ -1,9 +1,9 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
-public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
-	}
+public class Coins
+{
+    public static void main(String[] args) 
+    {
+      int centsamount = Integer.parseInt(args [0]);
+      // calculating the amount of quearters and the remaining cents, and printing the full answer
+      System.out.println("Use " + centsamount/25 + " quarters and " + centsamount%25 + " cents");  
+   }
 }
--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,19 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
 public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+   public static void main (String[] args)
+   {
+      int bottom = Integer.parseInt(args[0]);// range bottom
+      int top = Integer.parseInt(args[1]);// range top
+      double rand1 = Math.random();// generates a double random 
+      int int1 = bottom + (int)(rand1*(top-bottom+1));// makes random int and in range
+      double rand2 = Math.random();// generates a double random 
+      int int2 = bottom + (int)(rand2*(top-bottom+1));// makes random int and in range
+      double rand3 = Math.random();// generates a double random 
+      int int3 = bottom + (int)(rand3*(top-bottom+1));// makes random int and in range
+      System.out.println (int1);
+      System.out.println (int2);
+      System.out.println (int3);
+      int minnum = Math.min(int1,Math.min(int2,int3));// calculating the min number between the three rands
+      System.out.println ("the minimal generates number was "+ minnum);
+
+   }
 }
