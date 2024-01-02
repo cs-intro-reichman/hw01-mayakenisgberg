c8b0a04496413af35d3514a968b738912fe355f3
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-02 21:36:34 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,7 +3,9 @@ public class AddTwo {
       // Performs adding calculation on two commnand-line arguments
       int a = Integer.parseInt(args[0]);
       int b = Integer.parseInt(args[1]);
-      System.out.println(a + " + " + b + " = " + (a + b));
+      // #Feedback: better to not do calculation in print commands
+      int sum = a + b;
+      System.out.println(a + " + " + b + " = " + sum);
       
    }
 }
